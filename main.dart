4f57c7d637f50374aede4dbc237b41dcda411100
daniellepaschal <danielleonyemaobi07@gmail.com>
2026-02-07 14:31:02 -0800

    Refactor app structure and add habit tracking feature

--- main.dart
+++ main.dart
@@ -1,28 +1,67 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HabitApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HabitApp extends StatelessWidget {
+  const HabitApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'HELLO HABIT APP',
-            style: TextStyle(fontSize: 30),
-          ),
-        ),
-      ),
+      home: HabitPage(),
     );
   }
 }
 
+class HabitPage extends StatefulWidget {
+  const HabitPage({super.key});
 
+  @override
+  State<HabitPage> createState() => _HabitPageState();
+}
 
+class _HabitPageState extends State<HabitPage> {
+  List<String> habits = ['Pray', 'Workout', 'Read', 'Code'];
+  List<bool> completed = [false, false, false, false];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Habit Tracker'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: habits.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              habits[index],
+              style: TextStyle(
+                fontSize: 18,
+                decoration: completed[index]
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
+            trailing: Icon(
+              completed[index]
+                  ? Icons.check_circle
+                  : Icons.circle_outlined,
+              color: completed[index] ? Colors.green : Colors.grey,
+            ),
+            onTap: () {
+              setState(() {
+                completed[index] = !completed[index];
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+}
 
